c2bb4b63e5f3af503c3826379f19323835488607
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-08-22 21:37:27 +0300

    Added windicss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import windicss from "astro-windicss";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [windicss()],
+});
