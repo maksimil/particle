d43b237f8e9cce0e75301dc732b18b7223f2711c
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-08-27 02:06:01 +0300

    Styled main page

---
+++ windicss.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "windicss/helpers";
+
+export default defineConfig({
+  theme: {
+    extend: {
+      colors: {
+        black: "rgb(61, 60, 66)",
+        dark: "rgb(127, 82, 131)",
+        light: "rgb(166, 209, 230)",
+        white: "rgb(254, 251, 246)",
+      },
+    },
+  },
+});
