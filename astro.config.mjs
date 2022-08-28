5137d2dcf2b6799e2497072e899c5fa797c538ad
Maxim Kosterov <maxim.kosterov@gmail.com>
2022-08-28 17:06:50 +0300

    Started adding article page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 import { defineConfig } from "astro/config";
 import windicss from "astro-windicss";
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [windicss()],
+  vite: {
+    build: {
+      rollupOptions: {
+        plugins: [
+          yaml({
+            transform(data, file) {
+              return { data: { data, file } };
+            },
+          }),
+        ],
+      },
+    },
+  },
 });
---
+++ data/articles/test/index.md
@@ -0,0 +1,10 @@
+---
+title: article
+author: KsK<a@gmail.com>
+chapters:
+	- first.md
+	- second.md
+---
+
+Lorem ipsum dolor net
+Llllorem ipp
